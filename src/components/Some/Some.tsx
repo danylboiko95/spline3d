1954fe64accd57f9e7a9e132cfaa3c75442da731
danylboiko95 <danylboiko@gmail.com>
2021-05-15 20:39:53 +0300

    some

--- src/components/Some/Some.tsx
+++ src/components/Some/Some.tsx
@@ -7,10 +7,7 @@ interface IPropsSome {}
 const Some: React.FC<IPropsSome> = (props) => {
   return (
     <div className="Some">
-      <iframe
-        src="https://my.spline.design/libraryfloppy-f6a3a9eae4207ff6aa8f8c41e15c4c68/"
-        frameBorder="0"
-      ></iframe>
+     <iframe src='https://my.spline.design/libraryfloppy-f6a3a9eae4207ff6aa8f8c41e15c4c68/' frameBorder='0' width='100%' height='100%'></iframe>
       <NavLink to="/main">Link to portfolio</NavLink>
     </div>
   );
